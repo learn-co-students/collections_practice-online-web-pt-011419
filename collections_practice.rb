22a071b399a1909dfca47636a1ed1164f6673cff
A Glazer <avigayilglazer@gmail.com>
2019-01-21 14:08:58 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(arr)
+arr.sort {|a, b| a <=> b}
+end
 
+def sort_array_desc(arr)
+arr.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(arr)
+  arr.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(arr)
+  arr[0], arr[1], arr[2] = arr[0], arr[2], arr[1]
+end
